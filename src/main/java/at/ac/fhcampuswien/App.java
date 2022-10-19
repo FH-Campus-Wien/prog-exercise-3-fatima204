4f4d0cdad717898acf11a9000ce0a657ecacfa79
fatima204 <fatima.amrin.hossain@gmail.com>
2022-10-19 21:52:27 +0200

    not finished

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,13 +4,34 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-
     // Implement all methods as public static
-
     public static void main(String[] args) {
-        // test your method implementations here
-        // make method calls
-        // print their results
-        // etc.
+        oneMonthCalendar(30, 1);
+    //        // test your method implementations here
+    //        // make method calls
+    //        // print their results
+    //        // etc.
     }
+    public static void oneMonthCalendar (int numberofdays, int daybegin) {
+
+        int day = 2 - daybegin;
+
+        for(int i = 1; i <= numberofdays + daybegin - 1; i++){
+
+            if(i % 7 == 1){
+                System.out.println();
+            }
+            if(i < daybegin){
+                System.out.print("   ");
+            }else {
+                if (day / 10 == 0){
+                    System.out.print("  "+day);
+                }else {
+                    System.out.print(" "+day);
+                }
+            } day ++;
+        }
+    }
+
+
 }
