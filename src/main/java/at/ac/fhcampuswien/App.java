da44481b877ca2f02f3158c47f6f52d22df09851
fatima204 <fatima.amrin.hossain@gmail.com>
2022-10-21 15:29:09 +0200

    not finished

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,32 +4,84 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-    // Implement all methods as public static
     public static void main(String[] args) {
-        oneMonthCalendar(30, 1);
-    //        // test your method implementations here
-    //        // make method calls
-    //        // print their results
-    //        // etc.
+        oneMonthCalendar(30, 2);
+        //guessingGame(0);
+        //int a [] = {1,2,3,4,5,6};
+        //int b [] = {7,8,9,10,11};
+        //swapArrays();
     }
+
     public static void oneMonthCalendar (int numberofdays, int daybegin) {
 
-        int day = 2 - daybegin;
+        int day = 1;
 
         for(int i = 1; i <= numberofdays + daybegin - 1; i++){
 
-            if(i % 7 == 1){
+            if(i % 7 == 1) {
                 System.out.println();
             }
             if(i < daybegin){
                 System.out.print("   ");
             }else {
-                if (day / 10 == 0){
-                    System.out.print("  "+day);
-                }else {
-                    System.out.print(" "+day);
+                if (day / 10 == 0) {
+                    System.out.print("  " + day);
+                } else {
+                    System.out.print(" " + day);
                 }
-            } day ++;
+                day++;
+            }
+        }
+    }
+
+    public static void guessingGame (int numberToGuess) {
+
+        int number = 1;
+
+        while (true) {
+
+            if (number <= 10) {
+
+                Random random = new Random();
+                numberToGuess = random.nextInt(100) + 1;
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Guess number " + (number++) + ": ");
+                int Playerguess = scanner.nextInt();
+
+                if (Playerguess == numberToGuess) {
+                    System.out.println("You won wisenheimer!");
+                    break;
+                } else if (Playerguess < numberToGuess) {
+                    System.out.println("The number AI picked is higher than your guess.");
+                } else if (Playerguess > numberToGuess) {
+                    System.out.println("The number AI picked is lower than your guess.");
+                }
+            }else{
+            System.out.println("You lost! Have you ever heard of divide or conquer?");
+            break;
+            }
+        }
+    }
+
+    public static void swapArrays ( int array1, int array2){
+
+        int i;
+        int j;
+        int swap;
+
+        int a [] = {};
+        int b [] = {};
+
+        for(i=0; i< a.length; i++){
+            for(j=0; j< b.length; j++) {
+                swap = a[i];
+                a[i] = b[j];
+                b[j] = swap;
+
+                System.out.println(a[i]);
+                System.out.println(b[i]);
+            }
         }
     }
 
